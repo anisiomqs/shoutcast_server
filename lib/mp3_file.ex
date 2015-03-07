80a7b620fd7e0aedf0676516811446618c632b3a
Anisio Marques Junior <anisiomarxjr@gmail.com>
2015-03-07 11:27:45 -0400

    Add aditional test

--- lib/mp3_file.ex
+++ lib/mp3_file.ex
@@ -1,9 +1,12 @@
 defmodule Mp3File do
-  def extract_id3(file) do
+  def extract_metadata(file) do
     read_file = File.read!(file)
     file_length = byte_size(read_file)
     music_data = file_length - 128
     << _ :: binary-size(music_data), id3_section :: binary >> = read_file
-    IO.inspect id3_section
+    id3_section
+  end
+
+  def parse_id3(id3) do
   end
 end
--- test/mp3_file_test.exs
+++ test/mp3_file_test.exs
@@ -1,8 +1,19 @@
 defmodule Mp3FileTest do
   use ExUnit.Case
 
+  # test "returns the ID3 info" do
+  #   id3 = Mp3File.extract_id3("./test/nederland.mp3")
+  #   assert id3.artist == "Devil's Slingshot"
+  # end
+
   test "returns the ID3 info" do
-    id3 = Mp3File.extract_id3("./test/nederland.mp3")
-    assert id3.artist == "Devil's Slingshot"
+    metadata = Mp3File.extract_metadata("./test/nederland.mp3")
+    assert String.starts_with?(metadata, "TAG")
   end
+
+  test "returns the metadata" do
+    metadata = Mp3File.extract_metadata("./test/15-English Civil War.mp3")
+    assert String.starts_with?(metadata, "TAG")
+  end
+
 end
