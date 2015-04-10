7ff15011894400876d01ae149d84263547d8efd8
Marcio Junior <marciojunior_eu@yahoo.com.br>
2015-04-10 16:39:54 -0300

    Included socket dependency

--- mix.exs
+++ mix.exs
@@ -12,7 +12,8 @@ defmodule ShotcastServer.Mixfile do
   #
   # Type `mix help compile.app` for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:logger],
+     mod: {ShotcastServer, []}]
   end
 
   # Dependencies can be Hex packages:
@@ -25,6 +26,6 @@ defmodule ShotcastServer.Mixfile do
   #
   # Type `mix help deps` for more examples and options
   defp deps do
-    []
+    [{:socket, "~> 0.2"}]
   end
 end
